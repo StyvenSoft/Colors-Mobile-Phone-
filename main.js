ee39e157478e2ce637e0db83d5ecb5244e249bf0
Steveen <steveen.echeverri@gmail.com>
2020-01-20 12:28:17 -0500

    Decorations buttons

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 window.onload = () =>{
-    
+    const capacity_btn = document.querySelectorAll('.capacity .size');
+
+    for(let i = 0; i < capacity_btn.length; i++){
+        let btn = capacity_btn[i];
+        btn.addEventListener('click', function(){
+            document.querySelector('.capacity .size.selected').classList.remove('selected');
+            this.classList.add('selected');
+        })
+    }
 }
