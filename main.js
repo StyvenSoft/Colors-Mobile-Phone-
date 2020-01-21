02d635bbc70019faad3f63cc0e8ef832c4006c8e
Steveen <steveen.echeverri@gmail.com>
2020-01-20 20:31:33 -0500

    Applied images and colors

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 window.onload = () =>{
+    const colour_btn = document.querySelectorAll('.colours .colour');
     const capacity_btn = document.querySelectorAll('.capacity .size');
+    const imagery = document.querySelector('.imagery');
+    const image = document.querySelector('.imagery .image');
+    
 
     for(let i = 0; i < capacity_btn.length; i++){
         let btn = capacity_btn[i];
         btn.addEventListener('click', function(){
             document.querySelector('.capacity .size.selected').classList.remove('selected');
             this.classList.add('selected');
+        })
+    }
+    for(let i = 0; i < colour_btn.length; i++){
+        let btn = colour_btn[i];
+        btn.addEventListener('click', function(){
+            document.querySelector('.colours .colour.selected').classList.remove('selected');
+            this.classList.add('selected');
+            image.src = "images/phone-" + this.dataset.name + '.png';
+            imagery.style.background = this.dataset.colour;
         })
     }
 }
